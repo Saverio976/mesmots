ee6c274359d8e970b2d5a97e22e23a241d78a931
Xavier Mitault <xavier.mitault@epitech.eu>
2026-02-02 14:46:03 +0100

    Update tests.py

--- mesmots/tests.py
+++ mesmots/tests.py
@@ -1,7 +1,19 @@
+import unittest
 from mots import Mots
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_read_data(self):
+        mots = Mots()
+
+    def test_search(self):
+        mots = Mots()
+        r1 = mots.endswith(mots.tail(mots.split("saisissant")[0], 2))
+        res = mots.apply(r1)
+        self.assertEqual(len(res), 2)
 
 if __name__ == "__main__":
     print("import OK")
-    mots = Mots()
-    r1 = mots.endswith(m.tail(m.split("saisissant")[0], 2))
-    print("r1", m.apply(r1), "r1")
+    print("Starting unittest")
+    unittest.main()
+    print("Ending unittest")
