ea339a270b72afc2f6a967bc02090a25a07d70aa
Xavier Mitault <xavier.mitault@epitech.eu>
2026-02-02 14:40:33 +0100

    Update tests.py

--- mesmots/tests.py
+++ mesmots/tests.py
@@ -1,6 +1,7 @@
-from training.SyllabTokenize import SyllabTokenize
-from training.SyllabDecoder import SyllabDecoder
 from mots import Mots
 
 if __name__ == "__main__":
     print("import OK")
+    mots = Mots()
+    r1 = mots.endswith(m.tail(m.split("saisissant")[0], 2))
+    print("r1", m.apply(r1), "r1")
