e660ad84c60989a8f68d19e70abfd94c2faf5c98
Xavier Mitault <xavier.mitault@epitech.eu>
2026-02-02 14:55:05 +0100

    Update tests.py

--- mesmots/tests.py
+++ mesmots/tests.py
@@ -10,20 +10,24 @@ class TestStringMethods(unittest.TestCase):
         mots = Mots()
         r1 = mots.endswith(mots.tail(mots.split("saisissant")[0], 2))
         res = mots.apply(r1)
+        print("1", res)
         self.assertTrue(isinstance(res, list))
         self.assertNotEqual(len(res), 0)
 
     def test_split(self):
         mots = Mots()
         res = mots.syllab_tokenize.tokenize("macron")
+        print("2", res)
         self.assertTrue(isinstance(res, list))
         self.assertNotEqual(len(res), 0)
         
         res = mots.syllab_decoder.get(orthosyll="bon", ortho_before="", ortho_after="jour")
+        print("3", res)
         self.assertTrue(isinstance(res, list))
         self.assertNotEqual(len(res), 0)
         
         res = mots.split("bonjour")
+        print("4", res)
         self.assertTrue(isinstance(res, list))
         self.assertNotEqual(len(res), 0)
         
